f8a6489d4421f7356b9817cacaf145f3ffa3520e
ravisithiraju <ravi.sithiraju@sandstone.com.au>
2024-07-03 22:06:53 +1000

    corrected check for env.prod for mockservice

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,6 @@ import { provideClientHydration } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { provideHttpClient } from '@angular/common/http';
 import { MockDataService } from './services/mock-data.service';
-// import {MockDataService} from "./services/mock-data.service";
 import { createServer } from 'miragejs';
 import { provideStore } from '@ngrx/store';
 import { metaReducers, reducers } from './reducers';
@@ -12,13 +11,13 @@ import { FoodieEffects } from './effects/foodie.effects';
 import { provideStoreDevtools } from '@ngrx/store-devtools';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
-import { OAuthModule, provideOAuthClient } from 'angular-oauth2-oidc';
+import { provideOAuthClient } from 'angular-oauth2-oidc';
 import { SocialAuthServiceConfig } from '@abacritt/angularx-social-login';
 import { GoogleLoginProvider } from '@abacritt/angularx-social-login';
 import { environment } from '../environments/environment';
 
 export function initializeMockData(mockService: MockDataService) {
-  if(environment.production) {
+  if(!environment.production) {
     createServer({
       routes(): void {
         mockService.setRouteHandlers(this);
