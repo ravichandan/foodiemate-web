0ff9445d48d9f7bf096fbeb7d99dfcb0cb7241d2
Ravi Sithiraju <Ravi.Sithiraju@sandstone.com.au>
2024-11-17 23:23:35 +1100

    no matching items corrected

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { AppService } from './services/app.service';
 import { LeftNavigationComponent } from './left-navigation/left-navigation.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { HoverClassDirective } from './directives/hover-class.directive';
-import { Observable, Subject, take, takeUntil, tap } from 'rxjs';
+import { delay, Observable, Subject, take, takeUntil, tap } from 'rxjs';
 import { State } from './reducers';
 import { Store } from '@ngrx/store';
 import * as FoodieActions from './actions/foodie.actions';
@@ -94,7 +94,7 @@ export class AppComponent implements OnInit, OnDestroy {
     @Inject(PLATFORM_ID) platformId: Object,
     private readonly geolocation$: GeolocationService,
     private router: Router) {
-      geolocation$.subscribe(position => {
+      geolocation$.pipe(delay(3000),take(3)).subscribe(position => {
         console.log('JSON.string(position):: ', JSON.stringify(position));
         this.store.dispatch(FoodieActions.updateLocation({location:{latitude: position.coords?.latitude, longitude: position.coords?.longitude}} ))
         this.appService.getSuburbNameFromLocation(position.coords?.latitude.toString(), position.coords?.longitude.toString()).pipe(
